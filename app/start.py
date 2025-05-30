956dc696f7e53b336dab7c76514c1e5426592066
TS <tsependa_s@mail.ru>
2025-05-30 14:48:24 +0300

    Remove local_dir_use_symlinks parameter from snapshot_download in start.py

--- app/start.py
+++ app/start.py
@@ -8,7 +8,6 @@ snapshot_download(
     repo_id="black-forest-labs/FLUX.1-dev",
     token=HF_TOKEN,
     local_dir="/app/flux1-dev",
-    local_dir_use_symlinks=False,
     ignore_patterns=[
         "flux1-dev.safetensors",
         "ae.safetensors"
